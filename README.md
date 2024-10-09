8680c05bdf94b34da305db8e1f3f0d3fc1822942
chengfeng30121 <chengfeng3012@163.com>
2024-10-10 00:10:05 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ffmpeg_download_api
--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-def progress_version(filenames: [str, str, ...]) -> list:
+## vars
+
+debug = 0
+
+## Libs
+
+import requests
+import json
+import os
+
+def _get_info() -> dict:
+    if os.path.exists("info.json") and debug:
+        return json.loads(open("info.json").read())
+    url = "https://api.github.com/BtbN/FFmpeg-Builds/releases/latest"
+    response = requests.get(url)
+    return response.json()
+
+def progress_version(filenames: [(str, str), ...]) -> list:
     data = []
-    for filename in filenames:
+    for filename, url in filenames:
         d = {}
         if filename.endswith(".zip"):
             filename = filename[:-4]
@@ -40,16 +57,29 @@ def progress_version(filenames: [str, str, ...]) -> list:
                 else:
                     d["license"] = part.lower()
                 continue
+        d["download_url"] = "https://github.moeyy.xyz/" + url
         data.append(d)
     return data
 
-if __name__ == "__main__":
-    filenames = [
-        "ffmpeg-n6.1.2-9-g4571c80b40-linuxarm64-lgpl-6.1.tar.xz",
-        "ffmpeg-n7.1-7-g63f5c007a7-win64-lgpl-shared-7.1.zip",
-        "ffmpeg-N-117414-gb9145fcab2-linux64-gpl-shared.tar.xz"
-    ]
+## Main_Script
 
-    for d in progress_version(filenames):
-        print(d)
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "使用说明(目前暂不支持浏览器, 请使用爬虫查看)\n\n本api仅收录最新版本的ffmpeg, 如要老版本请自行下载(也许以后会...?)\n本项目完全公益, 未收任何人的一针一线, 开发费用全由我个人承担, 请不要盗刷流量, 谢谢\nGithub: https://github.com/chengfeng30121/ffmpeg_download_api\n\n====================\nURL: /list_version/\nMethods: (GET)\nUsage: 返回一个ffmpeg列表\n====================\n"
+
+@app.route("/list_version")
+def list_version():
+    json_data = _get_info()
+    raw_datas = []
+    for raw_data in json_data["assets"]:
+        raw_datas.append((raw_data["name"], raw_data["browser_download_url"]))
+    datas = progress_version(raw_datas)
+    datas = {"Code": 200, "Release Name": json_data["name"], "data": datas}
+    return datas
+
+if debug:
+    app.run(host="0.0.0.0", port=5001, debug=debug)
